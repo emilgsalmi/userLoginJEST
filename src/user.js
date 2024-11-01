5cd78b3d78a0844b49ad7707a5e5efd4d000a9f8
emilgsalmi <emil.salmi@hotmail.se>
2024-11-01 16:41:53 +0100

    small change

--- src/user.js
+++ src/user.js
@@ -1,22 +1,22 @@
 const users = []
 
-function createUser(username, password) {
+function createUser (username, password) {
   const userExists = users.some((u) => u.username === username)
   if (userExists) {
     return 'User already exists.'
   }
-  
+
   users.push({ username, password })
   return 'User created successfully.'
 }
 
-function changePassword(username, oldPassword, newPassword) {
+function changePassword (username, oldPassword, newPassword) {
   const user = users.find((u) => u.username === username && u.password === oldPassword)
-  
+
   if (!user) {
     return 'Invalid username or password.'
   }
-  
+
   user.password = newPassword
   return 'Password changed successfully.'
 }
