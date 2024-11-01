0d29aaed9a52795b0f85a168199389ef0c0bb023
emilgsalmi <emil.salmi@hotmail.se>
2024-11-01 17:08:52 +0100

    fixing for lint

--- src/user.js
+++ src/user.js
@@ -1,48 +1,48 @@
 class User {
-    constructor(username, password) {
-      this.username = username;
-      this.password = password;
-    }
+  constructor (username, password) {
+    this.username = username
+    this.password = password
   }
+}
   
   const users = [];
   
   const createUser = (username, password) => {
     if (users.some((u) => u.username === username)) {
-      return 'Error: Username already exists.';
+      return 'Error: Username already exists.'
     }
   
-    const passwordReg = /^(?=.*[0-9])(?=.*[A-Z]).{8,}$/;
+    const passwordReg = /^(?=.*[0-9])(?=.*[A-Z]).{8,}$/
   
     if (!passwordReg.test(password)) {
-      return 'Error: Password does not meet requirements';
+      return 'Error: Password does not meet requirements'
     }
   
-    users.push(new User(username, password));
-    return 'User created successfully.';
+    users.push(new User(username, password))
+    return 'User created successfully.'
   };
   
   const changePassword = (username, oldPassword, newPassword) => {
-    const existingUser = users.find((u) => u.username === username);
+    const existingUser = users.find((u) => u.username === username)
   
-    if (!existingUser) return 'Error: User does not exist';
+    if (!existingUser) return 'Error: User does not exist'
   
     if (existingUser.password.trim() !== oldPassword.trim()) {
-      return 'Error: Incorrect old password';
+      return 'Error: Incorrect old password'
     }
   
     if (oldPassword.trim() === newPassword.trim()) {
-      return 'Error: New password cannot be the same as the old password';
+      return 'Error: New password cannot be the same as the old password'
     }
   
     const passwordReg = /^(?=.*[0-9])(?=.*[A-Z]).{8,}$/;
     if (!passwordReg.test(newPassword.trim())) {
-      return 'Error: New password does not meet requirements';
+      return 'Error: New password does not meet requirements'
     }
   
-    existingUser.password = newPassword.trim();
-    return 'Password changed successfully';
+    existingUser.password = newPassword.trim()
+    return 'Password changed successfully'
   };
   
-  module.exports = { User, createUser, changePassword, users };
+  module.exports = { User, createUser, changePassword, users }
   
